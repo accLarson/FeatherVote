cc21bd43b9840f38fc0644fbab33c05ab3afbf66
accLarson <acc.larson@gmail.com>
2023-04-06 16:07:03 -0400

    database improvements. tracking total and previous monthly votes.

--- src/main/java/com/zerek/feathervote/managers/DatabaseManager.java
+++ src/main/java/com/zerek/feathervote/managers/DatabaseManager.java
@@ -11,78 +11,80 @@ import java.sql.SQLException;
 public class DatabaseManager {
 
     private static Connection connection;
+
     private final FeatherVote plugin;
-    private File file;
 
     public DatabaseManager(FeatherVote plugin) {
+
         this.plugin = plugin;
+
         this.initConnection();
+
         this.initTables();
-    }
-
-    public Connection getConnection() {
-        try {
-            if(connection.isClosed()) {
-                this.initConnection();
-            }
-        } catch (SQLException e) {
-            plugin.getLogger().severe("Unable to receive connection.");
-        }
-        return connection;
-    }
-
-    public void close() {
-        if (connection != null) {
-            try {
-                Base.close();
-                connection.close();
-            } catch (SQLException e) {
-                plugin.getLogger().severe("Unable to close DatabaseManager connection.");
-            }
-        }
     }
 
     private void initConnection() {
         File folder = this.plugin.getDataFolder();
-        if(!folder.exists()) {
-            folder.mkdir();
-        }
-        this.file = new File(folder.getAbsolutePath() + File.separator +  "FeatherVote.db");
+
+        File file = new File(folder.getAbsolutePath() + File.separator + "FeatherVote.db");
+
         try {
-            this.connection = DriverManager.getConnection("jdbc:sqlite:" + this.file.getAbsolutePath());
-            Base.attach(this.connection);
-        } catch (SQLException e) {
+
+            connection = DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
+
+            Base.attach(connection);
+        }
+
+        catch (SQLException e) {
+
             plugin.getLogger().severe("Unable to initialize DatabaseManager connection.");
         }
     }
 
-    private boolean existsTable(String table) {
+    private boolean existsTable() {
+
         try {
+
             if(!connection.isClosed()) {
-                ResultSet rs = connection.getMetaData().getTables(null, null, table, null);
+
+                ResultSet rs = connection.getMetaData().getTables(null, null, "VOTES", null);
+
                 return rs.next();
-            } else {
-                return false;
             }
-        } catch (SQLException e) {
+
+            else return false;
+        }
+
+        catch (SQLException e) {
+
             plugin.getLogger().severe("Unable to query table metadata.");
+
             return false;
         }
     }
 
     private void initTables() {
-        if(!this.existsTable("VOTES")) {
+
+        if(!this.existsTable()) {
+
             plugin.getLogger().info("Creating VOTES table.");
+
             String query = "CREATE TABLE IF NOT EXISTS `VOTES` ("
-                    + " `mojang_uuid`               VARCHAR(255) PRIMARY KEY NOT NULL, "
-                    + " `updated_at`                DATETIME, "
-                    + " `vote_count`                INT );";
+                    + " `mojang_uuid`                   VARCHAR(255) PRIMARY KEY NOT NULL, "
+                    + " `updated_at`                    DATETIME, "
+                    + " `vote_count_total`              INT, "
+                    + " `vote_count_previous_month`     INT, "
+                    + " `vote_count_current_month`      INT );";
+
             try {
-                if(!connection.isClosed()) {
-                    connection.createStatement().execute(query);
-                }
-            } catch (SQLException e) {
+
+                if(!connection.isClosed()) connection.createStatement().execute(query);
+            }
+
+            catch (SQLException e) {
+
                 e.printStackTrace();
+
                 plugin.getLogger().severe("Unable to create VOTES table.");
             }
         }
